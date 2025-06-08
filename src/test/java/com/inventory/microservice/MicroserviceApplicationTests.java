af9ee1508ae7710dba94eac59b37a022c15bf395
epjava7 <ericpanjava7@gmail.com>
2025-06-07 20:38:21 -0700

    tests point to right class

--- src/test/java/com/inventory/microservice/MicroserviceApplicationTests.java
+++ src/test/java/com/inventory/microservice/MicroserviceApplicationTests.java
@@ -3,7 +3,7 @@ package com.inventory.microservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.stock.ms.StockMsApplication.class)
 class MicroserviceApplicationTests {
 
 	@Test
